515a3efa65d892fd3bd28398318a9f17cc46b42c
Rivaldi Aliando <rivdew@gmail.com>
2022-05-09 13:32:00 +0700

    test

---
+++ TestPreview/TestPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPreviewApp.swift
+//  TestPreview
+//
+//  Created by Rivaldi Aliando on 29/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TestPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
